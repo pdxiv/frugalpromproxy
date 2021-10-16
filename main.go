8a33afd6c0074defa2daafdf3ed5420cadd730d1
pdxiv <pdxiv@github.com>
2021-10-16 23:06:37 +0200

    merge variable declaration with assignment

--- main.go
+++ main.go
@@ -240,8 +240,7 @@ func listener(queryPort, listenport int) {
 func WaitForCtrlC() {
 	var end_waiter sync.WaitGroup
 	end_waiter.Add(1)
-	var signal_channel chan os.Signal
-	signal_channel = make(chan os.Signal, 1)
+	signal_channel := make(chan os.Signal, 1)
 	signal.Notify(signal_channel, os.Interrupt)
 	go func() {
 		<-signal_channel
